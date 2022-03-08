43fb366b5063eed9add7580b71def63a2a61e13a
TC Lee <81542713+TCLee-tech@users.noreply.github.com>
2022-03-08 21:46:50 +0800

    StdIn-StdOut-I

---
+++ StdIn-StdOut-I/Stdlib.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import stdlib.StdIn;
+import stdlib.StdOut;
+
+public class Stdlib {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        
+        int a = StdIn.readInt();
+        int b = StdIn.readInt();
+        int c = StdIn.readInt();
+        
+
+        StdOut.println(a);
+        StdOut.println(b);
+        StdOut.println(c);
+    }
+}
+
